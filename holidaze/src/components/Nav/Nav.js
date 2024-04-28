b1a488ea906979bfcf3cae5369879e88d1cefd55
mhaarseth <mhaarseth@gmail.com>
2024-04-28 17:32:51 +0200

    Installs react-icons package

--- holidaze/src/components/Nav/Nav.js
+++ holidaze/src/components/Nav/Nav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@awesome.me/kit-KIT_CODE/icons/classic/solid";
+import { FaHome } from "react-icons/fa";
 import styles from "./Nav.module.css";
 
 export default function Nav() {
@@ -10,7 +9,7 @@ export default function Nav() {
       <h1 className={styles.navTitle}>Holidaze</h1>
       <div className={styles.menuContainer}>
         <Link to="/home" className={styles.menuItem}>
-          <faHouse />
+          <FaHome />
         </Link>
         <Link to="/profile" className={styles.menuItem}>
           Profile
