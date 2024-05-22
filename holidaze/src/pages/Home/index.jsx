d51e37367f90908d65d554870839411602e23f54
mhaarseth <mhaarseth@gmail.com>
2024-05-22 09:47:43 +0200

    Adds sorting options

--- holidaze/src/pages/Home/index.jsx
+++ holidaze/src/pages/Home/index.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useApi } from "../../hooks/useApi";
 import { ALL_VENUES_URL } from "../../constants/constants";
 import { Link } from "react-router-dom";
-import SearchField from "../../components/SearchField/SearchField";
 import styles from "./Home.module.css";
 
 export default function Home() {
-  const { data, isLoading, isError } = useApi(ALL_VENUES_URL);
+  const [sortField, setSortField] = useState("created");
+  const [sortOrder, setSortOrder] = useState("desc");
+  const sortedResults =
+    ALL_VENUES_URL + `?sort=${sortField}&sortOrder=${sortOrder}`;
+  const { data, isLoading, isError } = useApi(sortedResults);
   console.log(data);
 
   if (isLoading) {
@@ -16,11 +19,44 @@ export default function Home() {
     return <div>Error loading data</div>;
   }
 
+  const handleSortOrder = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  const handleSortField = (event) => {
+    setSortField(event.target.value);
+  };
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.homeContentContainer}>
         <div className={styles.headingContainer}>
           <h2 className={styles.homeHeading}>All venues</h2>
+          <div className={styles.sortingFields}>
+            <div>
+              <label htmlFor="sortOrder">Order by: </label>
+              <select
+                id="sortOrder"
+                value={sortOrder}
+                onChange={handleSortOrder}
+              >
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="sortField">Sort by: </label>
+              <select
+                id="sortField"
+                value={sortField}
+                onChange={handleSortField}
+              >
+                <option value="created">Created</option>
+                <option value="name">Name</option>
+                <option value="price">Price</option>
+              </select>
+            </div>
+          </div>
         </div>
         <div className={styles.container}>
           {data.map((venue) => (
